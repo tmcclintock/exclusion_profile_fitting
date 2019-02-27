b17dc2e22ff046b4e5f39ddf5c5bf053d11950a7
Tom McClintock <tmcclintock89@gmail.com>
2019-02-27 05:45:31 -0800

    First commit

---
+++ profile_fitter/profile_fitter.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy.optimize as op
+import emcee
+import aemulus_extras as ae
+import aemulus_data as ad
+
+class fitter(object):
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    f = fitter()
---
+++ tests/test_fitter.py
@@ -0,0 +1,6 @@
+import pytest
+import numpy as np
+import numpy.testing as npt
+
+def test_fitter_builds():
+    pass
