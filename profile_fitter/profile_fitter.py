87d13c95fe5a9133a8bda41307dc56ac249e1eca
Tom McClintock <tmcclintock89@gmail.com>
2019-03-07 19:08:34 -0500

    Working more on the fitter.

--- profile_fitter/profile_fitter.py
+++ profile_fitter/profile_fitter.py
@@ -18,9 +18,28 @@ class SingleSnapshotFitter(object):
             np.load("covs_Box%d_Z%d.npy"%(box, snapshot))
         self.data_dict["masses"] = \
             np.load("datafiles/masses_Box%d_Z%d.npy"%(box, snapshot))
-
-    def lnlike(self, params, r, xihm, icov):
+        self.extras = ae.Extras(box)
+        self.redshifts = 1./ad.scale_factors() - 1.
         
+    def lnlike(self, params, M, r, xihm, icov):
+        #Also has the prior
+        conc, rt, beta, nua, nub = np.exp(params)
+        if nua < nub:
+            return -1e-99
+        Ma = self.M_nu_spline(nua)
+        Mb = self.M_nu_spline(nub)
+        ca = self.c_nu_spline(nua)
+        cb = self.c_nu_spline(nub)
+        bias = self.bias
+        xi_nl = self.xi_nl
+        Omega_m = self.Omega_m
+        xi_ex = ct.exclusion.xi_hm_exclusion_at_r(r, M, conc, rt, beta, Ma, ca,
+                                                  Mb, cb, bias, xi_nl, Omega_m,
+                                                  exclusion_scheme=0)
+        X = xihm - xi_ex
+        return -0.5*np.dot(X, np.dot(icov, X))
+
+    
         
     def perform_best_fit(self, quiet=False):
         
